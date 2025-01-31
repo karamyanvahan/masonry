0dc89d93c5af97dff15b78b9e72a8ab08e1d0e08
Vahan Karamyan <karamyanvahan@gmail.com>
2025-01-31 20:59:02 +0400

    enhance MasonryItem component with loading placeholder and dynamic height adjustment

--- src/components/Masonry/MasonryItem.tsx
+++ src/components/Masonry/MasonryItem.tsx
@@ -1,8 +1,8 @@
 import { PexelsPhoto } from "api/types";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-const StyledMasonryGridItem = styled.div`
+const StyledMasonryGridItem = styled.div<{ isLoading: boolean }>`
   position: relative;
   margin: 5px 0;
 
@@ -26,18 +26,48 @@ const StyledMasonryGridItem = styled.div`
   }
 
   .placeholder {
+    pointer-events: none;
     position: absolute;
     width: 100%;
+    transition: opacity 0.2s;
+    opacity: ${(props) => (props.isLoading ? 1 : 0)};
   }
 `;
 
 export const MasonryItem: React.FC<{
   photo: PexelsPhoto;
 }> = ({ photo }) => {
+  const placeholderEl = useRef<HTMLDivElement>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [placeholderHeight, setPlaceholderHeight] = useState(0);
+
+  useEffect(() => {
+    if (placeholderEl.current?.clientWidth) {
+      setPlaceholderHeight(
+        (placeholderEl.current?.clientWidth / photo.width) * photo.height
+      );
+    }
+  }, [photo.height, photo.width]);
+
   return (
-    <StyledMasonryGridItem>
+    <StyledMasonryGridItem isLoading={isLoading}>
       <div className="overlay"></div>
-      <img alt={photo.alt} src={photo.src?.large} />
+      <div
+        className="placeholder"
+        ref={placeholderEl}
+        style={{
+          height: placeholderHeight + "px",
+          backgroundColor: photo.avg_color,
+        }}
+      ></div>
+      <img
+        height={isLoading ? placeholderHeight : undefined}
+        alt={photo.alt}
+        src={photo.src?.large}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+      />
     </StyledMasonryGridItem>
   );
 };
