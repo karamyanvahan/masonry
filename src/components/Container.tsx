c10178dd343b9c87cfffbf8807a69d1e40b7073a
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-01 05:03:04 +0400

    add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+
+  @media screen and (min-width: 1400px) {
+    max-width: 1400px;
+  }
+
+  @media screen and (max-width: 1399px) {
+    max-width: 1200px;
+  }
+  @media screen and (max-width: 1199px) {
+    max-width: 992px;
+  }
+  @media screen and (max-width: 991px) {
+    max-width: 768px;
+  }
+  @media screen and (max-width: 767px) {
+    max-width: 576px;
+  }
+  @media screen and (max-width: 575) {
+    max-width: 100%;
+  }
+`;
