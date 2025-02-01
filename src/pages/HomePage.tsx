8ec2d61bc16db38a2bafb7af8371ed7a351e79ca
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-01 21:48:21 +0400

    refactor HomePage to use search parameters for query handling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import { Masonry } from "components/Masonry";
 import { Search } from "components/Search";
-import { useState } from "react";
+import { useSearchParams } from "react-router";
 
 export const HomePage: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <>
-      <Search onSearch={setSearchQuery} />
-      <Masonry searchQuery={searchQuery} />
+      <Search onSearch={(q) => setSearchParams({ q: q })} />
+      <Masonry searchQuery={searchParams.get("q") || ""} />
     </>
   );
 };
