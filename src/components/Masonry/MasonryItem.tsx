63d7a6cad5f57449c46d3efa1b3558f9ddb1dbbb
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-01 04:58:27 +0400

    refactor MasonryItem component to remove unused state and effect for placeholder height calculation

--- src/components/Masonry/MasonryItem.tsx
+++ src/components/Masonry/MasonryItem.tsx
@@ -1,6 +1,6 @@
 import { PexelsPhotoResponse } from "api/types";
 import { Image } from "components/Image";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Link } from "react-router";
 import styled from "styled-components";
 
@@ -13,24 +13,12 @@ const StyledMasonryGridItem = styled.div`
 export const MasonryItem: React.FC<{
   photo: PexelsPhotoResponse;
 }> = ({ photo }) => {
-  const imageEl = useRef<HTMLImageElement>(null);
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    const setPlaceholderHeight = () => {
-      if (imageEl.current?.clientWidth) {
-        setHeight((imageEl.current.clientWidth / photo.width) * photo.height);
-      }
-    };
-    setPlaceholderHeight();
-  }, [photo.height, photo.width]);
-
   return (
     <Link to={"/" + photo.id}>
       <StyledMasonryGridItem>
         <Image
-          ref={imageEl}
-          height={height}
+          width={photo.width}
+          height={photo.height}
           src={photo.src?.medium}
           alt={photo.alt}
           placeholderColor={photo.avg_color}
