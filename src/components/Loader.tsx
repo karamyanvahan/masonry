21292b0ed53b4da9da7ef4e9d856cd2b9496b4c0
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-01 19:45:15 +0400

    change loader animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,46 +1,42 @@
 import styled from "styled-components";
 
 export const Loader = styled.div`
-  width: 40px;
-  aspect-ratio: 1;
-  --c: linear-gradient(#000 0 0);
-  --r1: radial-gradient(farthest-side at bottom, #000 93%, #0000);
-  --r2: radial-gradient(farthest-side at top, #000 93%, #0000);
-  background: var(--c), var(--r1), var(--r2), var(--c), var(--r1), var(--r2),
-    var(--c), var(--r1), var(--r2);
-  background-repeat: no-repeat;
-  animation: l2 1s infinite alternate;
-
-  @keyframes l2 {
-    0%,
-    25% {
-      background-size: 8px 0, 8px 4px, 8px 4px, 8px 0, 8px 4px, 8px 4px, 8px 0,
-        8px 4px, 8px 4px;
-      background-position: 0 50%, 0 calc(50% - 2px), 0 calc(50% + 2px), 50% 50%,
-        50% calc(50% - 2px), 50% calc(50% + 2px), 100% 50%, 100% calc(50% - 2px),
-        100% calc(50% + 2px);
-    }
-    50% {
-      background-size: 8px 100%, 8px 4px, 8px 4px, 8px 0, 8px 4px, 8px 4px,
-        8px 0, 8px 4px, 8px 4px;
-      background-position: 0 50%, 0 calc(0% - 2px), 0 calc(100% + 2px), 50% 50%,
-        50% calc(50% - 2px), 50% calc(50% + 2px), 100% 50%, 100% calc(50% - 2px),
-        100% calc(50% + 2px);
-    }
-    75% {
-      background-size: 8px 100%, 8px 4px, 8px 4px, 8px 100%, 8px 4px, 8px 4px,
-        8px 0, 8px 4px, 8px 4px;
-      background-position: 0 50%, 0 calc(0% - 2px), 0 calc(100% + 2px), 50% 50%,
-        50% calc(0% - 2px), 50% calc(100% + 2px), 100% 50%, 100% calc(50% - 2px),
-        100% calc(50% + 2px);
-    }
-    95%,
+  height: 40px;
+  aspect-ratio: 0.866;
+  display: grid;
+  background: conic-gradient(
+    from -121deg at right,
+    #0000,
+    #bf1e62 1deg 60deg,
+    #0000 61deg
+  );
+  animation: l12 2s infinite linear;
+  transform-origin: 33% 50%;
+  &:before,
+  &:after {
+    content: "";
+    grid-area: 1/1;
+    background: conic-gradient(
+      from -121deg at right,
+      #0000,
+      #ffa588 1deg 60deg,
+      #0000 61deg
+    );
+    transform-origin: inherit;
+    animation: inherit;
+  }
+  &:after {
+    background: conic-gradient(
+      from -121deg at right,
+      #0000,
+      #027b7f 1deg 60deg,
+      #0000 61deg
+    );
+    animation-duration: 3s;
+  }
+  @keyframes l12 {
     100% {
-      background-size: 8px 100%, 8px 4px, 8px 4px, 8px 100%, 8px 4px, 8px 4px,
-        8px 100%, 8px 4px, 8px 4px;
-      background-position: 0 50%, 0 calc(0% - 2px), 0 calc(100% + 2px), 50% 50%,
-        50% calc(0% - 2px), 50% calc(100% + 2px), 100% 50%, 100% calc(0% - 2px),
-        100% calc(100% + 2px);
+      transform: rotate(1turn);
     }
   }
 `;
