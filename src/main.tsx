3b24afb4373e0c1feb19a7f9a7995b4bb81c7605
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-02 16:56:32 +0400

    feat: add ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "main.css";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  default: "#1a1a1a",
+  error: "rgb(221, 52, 6)",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
