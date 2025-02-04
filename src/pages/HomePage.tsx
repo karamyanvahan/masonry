301d80417c1e938bf379a8632c6eca1a9799b016
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-04 04:12:08 +0400

    refactor HomePage component to remove styled-components and directly set Masonry height

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,13 @@
 import { Masonry } from "components/Masonry";
 import { useSearchParams } from "react-router";
-import styled from "styled-components";
-
-const StyledPage = styled.div`
-  .grid-container {
-    height: calc(100vh - 67px);
-  }
-`;
 
 export const HomePage: React.FC = () => {
   const [searchParams] = useSearchParams();
 
   return (
-    <StyledPage>
-      <div className="grid-container">
-        <Masonry searchQuery={searchParams.get("q") || ""} />
-      </div>
-    </StyledPage>
+    <Masonry
+      height="calc(100vh - 90px)"
+      searchQuery={searchParams.get("q") || ""}
+    />
   );
 };
