2af48683cb6c4ea4cad61b8b092c0176d5f43620
Vahan Karamyan <karamyanvahan@gmail.com>
2025-02-05 03:01:06 +0400

    include google fonts css into build to reduce css files

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "fonts.css";
 import "main.css";
 import { ThemeProvider } from "styled-components";
 
